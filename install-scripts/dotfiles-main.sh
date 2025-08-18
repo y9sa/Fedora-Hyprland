110cfba49936700d7193aa02b9f03aaf6dcfd3b5
y9sa <y9sa10@gmail.com>
2025-08-18 08:17:06 +0300

    Update dotfiles-main.sh

--- install-scripts/dotfiles-main.sh
+++ install-scripts/dotfiles-main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 💫 https://github.com/JaKooLit 💫 #
+# 💫 https://github.com/JaKooLit 💫my #
 # Hyprland-Dots to download from main #
 
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
